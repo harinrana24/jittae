f4a07ccd20c2c87dc051eed1235687310eb15625
Harin Rana <harinrana24@gmail.com>
2024-04-30 03:36:17 -0400

    Updates

---
+++ assets/index-imyEFL6S.js
@@ -0,0 +1,2 @@
+const __vite__fileDeps=["assets/StarDrawer-CZnR-rNL.js","assets/index-QGWFl7Dt.js","assets/index-C-5JYWDM.css"],__vite__mapDeps=i=>i.map(i=>__vite__fileDeps[i]);
+import{_ as e}from"./index-QGWFl7Dt.js";async function _(a,t=!0){const{StarDrawer:r}=await e(()=>import("./StarDrawer-CZnR-rNL.js"),__vite__mapDeps([0,1,2]));await a.addShape("star",new r,t)}export{_ as loadStarShape};
--- assets/interaction-eR7BFvyK.js
+++
@@ -1,2 +0,0 @@
-const __vite__fileDeps=["assets/Linker-DzYdkKQa.js","assets/Ranges-ChRDEEO4.js","assets/index-Dsx1wIy0.js","assets/index-C-5JYWDM.css","assets/index-CfPcTpf4.js","assets/OptionsColor-rvHt8Mc7.js","assets/ParticlesInteractorBase-vfDeBun3.js"],__vite__mapDeps=i=>i.map(i=>__vite__fileDeps[i]);
-import{_ as i}from"./index-Dsx1wIy0.js";async function o(t,r=!0){await t.addInteractor("particlesLinks",async a=>{const{Linker:n}=await i(()=>import("./Linker-DzYdkKQa.js"),__vite__mapDeps([0,1,2,3,4,5,6]));return new n(a)},r)}export{o as loadLinksInteraction};
