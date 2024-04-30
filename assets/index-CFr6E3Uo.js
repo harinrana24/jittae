7c8194cbfc55288191f79472188d48030cf68e53
Harin Rana <harinrana24@gmail.com>
2024-04-30 03:29:01 -0400

    Updates

---
+++ assets/index-CFr6E3Uo.js
@@ -0,0 +1,2 @@
+const __vite__fileDeps=["assets/BaseMover-BUCDc0rE.js","assets/index-Dsx1wIy0.js","assets/index-C-5JYWDM.css"],__vite__mapDeps=i=>i.map(i=>__vite__fileDeps[i]);
+import{_ as r}from"./index-Dsx1wIy0.js";async function _(a,e=!0){await a.addMover("base",async()=>{const{BaseMover:o}=await r(()=>import("./BaseMover-BUCDc0rE.js"),__vite__mapDeps([0,1,2]));return new o},e)}export{_ as loadBaseMover};
---
+++ assets/index-DPsYCHiz.js
@@ -0,0 +1,2 @@
+const __vite__fileDeps=["assets/OutOfCanvasUpdater-Bw2aj8re.js","assets/index-Dsx1wIy0.js","assets/index-C-5JYWDM.css"],__vite__mapDeps=i=>i.map(i=>__vite__fileDeps[i]);
+import{_ as o}from"./index-Dsx1wIy0.js";async function i(t,a=!0){await t.addParticleUpdater("outModes",async e=>{const{OutOfCanvasUpdater:r}=await o(()=>import("./OutOfCanvasUpdater-Bw2aj8re.js"),__vite__mapDeps([0,1,2]));return new r(e)},a)}export{i as loadOutModesUpdater};
